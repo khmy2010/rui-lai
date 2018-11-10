e2a8c53c75d462107b8c94eb28e7d0d12dc532eb
khmy2010 <kehan.see@gmail.com>
2018-11-10 18:22:06 +0800

    added material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule.forRoot(),
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
